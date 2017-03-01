f136cb29c40444608cec9a61f993d4872bdd952f
bhrzali <bhrz.ali@gmail.com>
2017-03-01 23:26:47 +0430

    logistic regression

--- Breast_cancer_classification.r
+++ Breast_cancer_classification.r
@@ -1,31 +1,35 @@
 
 breast_data = read.csv("datasets/breast-cancer-wisconsin.csv",header=F,na.strings="?")
 breast_data = na.omit(breast_data)
+#Removing the first column. It contains the sample code number
 breast_data = breast_data[,-1]
+#Making the final attribute qualitative
+breast_data$V11 = as.factor(breast_data$V11)
+#The names of the columns of the attributes are arranged in the same order as above.
 names(breast_data)
 
 nrow(breast_data)
 
-breast_data$V11 = as.factor(breast_data$V11)
 set.seed(1)
+#creating a training dataset
 train = sample(nrow(breast_data),550)
+
 glm.fit = glm(V11~.,data=breast_data,family=binomial,subset=train)
 glm.prob = predict(glm.fit,newdata=breast_data[-train,],type="response")
 glm.pred = rep(2,length(glm.prob))
 glm.pred[glm.prob>0.5]=4
+
+#error rate
 mean(glm.pred!=breast_data[-train,]$V11)*100
 
 #accuracy
 mean(glm.pred==breast_data[-train,]$V11)*100
 
-summary(glm.fit)
+#confusion table 2=benign 4=malignant
+table(glm.pred,breast_data[-train,]$V11)
 
-#v3 - uniformity of cell size v5- uniformity of cell shape
-#find correlation between these attributes and the output
-cor(glm.prob,breast_data[-train,]$V3)
+91/(91+1)*100
 
-cor(glm.prob,breast_data[-train,]$V4)
-
-cor(glm.prob,breast_data[-train,]$V10)
+39/(39+2)*100
 
 
